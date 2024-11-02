f655347aea99ed9f83f4b0cb79b319c789f22139
yu-takahashi <36310653+yu-takahashi-314@users.noreply.github.com>
2024-11-02 13:06:43 +0900

    /api/usersでuserデータが返ってくるまで

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,10 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+User.create!(
+  [
+    { name: 'Alice', email: 'alice@example.com' },
+    { name: 'Bob', email: 'bob@example.com' },
+    { name: 'Charlie', email: 'charlie@example.com' }
+  ]
+)
